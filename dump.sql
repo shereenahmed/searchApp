a48e2d77ecfa0bba2a23992280d316546117c4ac
shereenahmed <shereen.albadry@gmail.com>
2016-10-28 11:34:03 +0200

    commit sql file

--- dump.sql
+++ dump.sql
@@ -14,3 +14,43 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `students` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(32) DEFAULT NULL,
+  `lastname` varchar(32) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `courses` varchar(64) DEFAULT NULL,
+  `teacher` varchar(64) DEFAULT NULL,
+  `duration` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `students`
+--
+
+LOCK TABLES `students` WRITE;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+INSERT INTO `students` VALUES (1,'Shereen','Ahmed',30,'Music','Marcus','20H'),(2,'Marie','Blue',23,'Math','Belly','10H'),(3,'Linda','Green',31,'Art','Ariel','5H'),(4,'Haidy','Brown',26,'Poetry','Mathew','12H'),(5,'Walter','White',33,'English','Peter','20H'),(7,'Wolfgang','Lehmann',47,'History','Nick','30H'),(8,'Bernice','Listz',20,'Art','Ariel','5H'),(9,'Vannisa','Ives',28,'Theatre','Monica','15H'),(10,'Lily','Rose',34,'Cooking','Edward','20H'),(11,'Antonio','Moreno',24,'German','Patrik','33H'),(12,'Mohamad','Hady',39,'Swedish','Olle','34H'),(13,'Adrian','Mina',20,'Dance','Julian','5H'),(14,'Anna','Gavaldi',19,'Writing','Mendy','12H');
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-10-28 11:33:13
